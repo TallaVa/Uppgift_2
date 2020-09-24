00a02c1ca6409626477abdeaf58ee9018a528e7e
Behram <behram89@gmail.com>
2020-09-24 23:45:21 +0200

    uppgift 4.5 . kan ej få att while yYnN ska avsluta loopen :(

--- src/com/TallaVa/com/TallaVa/FromTheBook.java
+++ src/com/TallaVa/com/TallaVa/FromTheBook.java
@@ -1,19 +1,52 @@
 package com.TallaVa;
 
+import javax.swing.JOptionPane;
+
 public class FromTheBook {
 
     public static void main(String [] args){
 
         // alla uppgifter inför inlämning2
 
+        /* CheckPoint 4.4
+        int number;
+        String input;
 
-        int B = 0;
+        input = JOptionPane.showInputDialog("Enter a number between 10 and 24: ");
+        number = Integer.parseInt(input);
 
-        while (B < 10)
+        while (number <10 || number > 24)
         {
-            System.out.println("YEY JAVA");
-            B++;
+            input = JOptionPane.showInputDialog("you are not in the range. " +
+                                                "please re enter a number in range: ");
+            number = Integer.parseInt(input);
         }
+
+        JOptionPane.showMessageDialog(null,"You have enterd: "+number);
+
+        System.exit(0);
+
+         */
+
+        // CheckPoint 4.5
+
+
+        String input;
+        char answer;
+
+        input = JOptionPane.showInputDialog("Write 'Y', 'y', 'N' or 'n' : ");
+        answer = input.charAt(0);
+
+        while ((answer != 'Y') || (answer != 'y'))
+        {
+            input = JOptionPane.showInputDialog("this is not the right character please try again: ");
+            answer = input.charAt(0);
+
+        }
+
+        JOptionPane.showMessageDialog(null, " You answerd :" +answer +" wich is correct");
+
+        System.exit(0);
     }
 
 }
