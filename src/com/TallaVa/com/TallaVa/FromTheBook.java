e28005029db1151ee4ab8c9939db69de092ac4fb
Behram <behram89@gmail.com>
2020-09-24 06:21:13 +0200

    Ny class för uppgifter i från boken

---
+++ src/com/TallaVa/com/TallaVa/FromTheBook.java
@@ -0,0 +1,13 @@
+package com.TallaVa;
+
+public class FromTheBook {
+
+    public static void main(String [] args){
+
+        // alla uppgifter inför inlämning2
+
+        System.out.println("lets begin :)");
+
+    }
+
+}
