1379dc6926919858deffc913b89479cddaa9635d
Behram <behram89@gmail.com>
2020-10-14 21:05:48 +0200

    programmed the Clock :)

--- src/com/TallaVa/com/TallaVa/FromTheBook.java
+++ src/com/TallaVa/com/TallaVa/FromTheBook.java
@@ -1,7 +1,7 @@
 package com.TallaVa;
 
 import javax.swing.JOptionPane;
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class FromTheBook {
 
@@ -56,7 +56,7 @@ public class FromTheBook {
          */
 
 
-        // Code Listing 4-10
+        /* Code Listing 4-10
 
         //Variables
         int days;           //the number of days
@@ -85,6 +85,84 @@ public class FromTheBook {
         JOptionPane.showMessageDialog(null, String.format("The sales are $%,.2f", totalSales));
 
         System.exit(0);
+
+         */
+
+        /* Code Listing 4-11
+        // Test av Sentinel Value
+
+        int points;
+        int totalPoints = 0;
+
+        //Create a scanner object for keyboard input.
+        Scanner keyboard = new Scanner(System.in);
+
+        //Display general instructions.
+        System.out.println("Enter the number of points your team\n" +
+                "has earned for each game this season.\n" +
+                "Enter -1 when finished.");
+
+        //Get the first number of points.
+        System.out.print("Enter game points or -1 to end: ");
+        points = keyboard.nextInt();
+
+        //Accumulate the points until -1 is entered.
+        while (points != -1)
+        {
+            //Add ponts to totalPoints.
+            totalPoints += points;
+
+            //Get the next number of points.
+            System.out.print("Enter game points or -1 to end: ");
+            points = keyboard.nextInt();
+        }
+
+        //Display the total number of points.
+        System.out.println("The total points are "+ totalPoints);
+
+         */
+
+        //CheckPoint
+
+            /* 4.13
+            int number;         //Store the number taken by the user
+            int totalNumber = 0;    //Store the totalSum
+
+            //Create new scanner
+            Scanner keyboard = new Scanner(System.in);
+
+            //Ask the user for a number.
+            System.out.println("Enter a number: ");
+            number = keyboard.nextInt();
+
+            for (int count = 1; count < 7; count++)
+            {
+                System.out.println("Enter a number: ");
+                number = keyboard.nextInt();
+                //Add points to totalSum
+                totalNumber += number;
+
+            }
+
+            System.out.print("The total sum is: " + totalNumber);
+
+            System.exit(0);
+
+             */
+        //The Clock program
+
+        // simulate the clock
+            for (int hours = 1; hours <= 1; hours++)
+            {
+                for (int minutes = 0; minutes <= 10; minutes ++)
+                {
+                    for (int seconds = 0; seconds <= 20; seconds ++)
+                    {
+                        System.out.printf("%02d:%02d:%02d\n", hours, minutes, seconds);
+                    }
+                }
+            }
+
     }
 
 }
