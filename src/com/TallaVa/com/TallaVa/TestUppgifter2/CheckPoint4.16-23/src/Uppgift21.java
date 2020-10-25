439d35b003c7ff3f2cfc8d2e49c28e9c5fed3635
Behram <behram89@gmail.com>
2020-10-25 18:34:52 +0100

    19 klar. 21 på G

---
+++ src/com/TallaVa/com/TallaVa/TestUppgifter2/CheckPoint4.16-23/src/Uppgift21.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Uppgift21 {
+    
+    public static void main(String[] args) throws IOException {
+    
+        //öppna fil och läser första raden.
+        File file = new File("Behram.txt");
+        Scanner inputFile = ne
+
+        
+    }
+}
