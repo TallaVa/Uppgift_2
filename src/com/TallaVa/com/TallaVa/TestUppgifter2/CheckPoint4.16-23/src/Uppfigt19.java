be7ac97bae9462957adc36a6b5ca2cf630f23484
Behram <behram89@gmail.com>
2020-10-25 17:39:38 +0100

    Övningsuppgift4-19 påbörjat

---
+++ src/com/TallaVa/com/TallaVa/TestUppgifter2/CheckPoint4.16-23/src/Uppfigt19.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Uppfigt19 {
+
+    public static void main(String [] args) throws IOException{
+        
+        System.out.println("behram");
+
+    }
+    
+}
