af3d14c8b38356ea3707edfd98feb6350620b5fa
Behram <behram89@gmail.com>
2020-10-27 20:22:34 +0100

    uppgift 21 klart

--- src/com/TallaVa/com/TallaVa/TestUppgifter2/CheckPoint4.16-23/src/Uppgift21.java
+++ src/com/TallaVa/com/TallaVa/TestUppgifter2/CheckPoint4.16-23/src/Uppgift21.java
@@ -1,14 +1,25 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Uppgift21 {
     
     public static void main(String[] args) throws IOException {
     
         //öppna fil och läser första raden.
-        File file = new File("Behram.txt");
+        File file = new File("MyName.txt");
         Scanner inputFile = new Scanner(file);
-        
+
+
+        //Läs första raden
+        String line = inputFile.nextLine();
+
+        //Visa det som scannern har läst från filen MyName.txt
+        System.out.println("Första raden från filen är: ");
+        System.out.println(line);
+
+        //Stänga filen 
+        inputFile.close();
 
         
     }
