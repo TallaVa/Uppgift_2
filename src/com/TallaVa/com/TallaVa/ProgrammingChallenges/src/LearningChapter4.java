2858afd376fc20dc893ce6375b0fea6ebd7e61a2
Behram <behram89@gmail.com>
2020-10-29 22:18:06 +0100

    uppgifter för kapitel 4

---
+++ src/com/TallaVa/com/TallaVa/ProgrammingChallenges/src/LearningChapter4.java
@@ -0,0 +1,23 @@
+public class LearningChapter4 {
+    public static void main(String[] args) throws Exception {
+        
+        
+        // Uppgift 4-1
+        
+        /* Sum of Numbers
+            användaren ska slå in ett tal mellan 1-20 och programmet ska 
+            summera allt upp till det heltalet användaren valt.
+        */
+
+        int number;     //Talet användaren valt
+        int sum;        //Minne som håller tal efter varje itteration
+
+        
+        //Skapa Scanner
+        
+        
+        System.out.println("Mata in ett tal mellan 1-20: ");
+
+    
+    }
+}
