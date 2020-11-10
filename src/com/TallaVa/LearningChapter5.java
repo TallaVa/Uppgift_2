ab6ac14ef1f9c0b1b9940494e252fda14ccff9e6
TallaVa <behram89@gmail.com>
2020-11-10 21:09:32 +0100

    new chapter

---
+++ src/com/TallaVa/LearningChapter5.java
@@ -0,0 +1,71 @@
+import javax.swing.JOptionPane;
+
+public class LearningChapter5 {
+    
+    public static void main(String [] args ){
+
+        
+        /** Test av en CodeListning 5-3 där vi använder oss av två void metoder 
+
+        double salary;      // lön
+        int creditRating;   //Credit rating
+        String input;       // användarens inslag
+        
+        // få in användarens lön
+        input = JOptionPane.showInputDialog("vad har du för lön?: ");
+        salary = Double.parseDouble(input);
+
+        // få in användarens credit rating ( 1 till 10)
+        input = JOptionPane.showInputDialog("på en skala mellan 1 till 10 " +
+                                            " vad har du för credit värdighet ?: " +
+                                            " 1 = dåligt och 10 = utmärkt. ");
+        creditRating = Integer.parseInt(input);
+
+        // Loop som kollar om man är vädrig att få credit kort
+        if (salary >= 20000 && creditRating >= 7)
+            qualify();
+        else
+            noQualify();
+        
+        System.exit(0);
+        */
+
+    
+
+    /**
+     * Metoden qualify informerar om man kan ta ut ett 
+     * credit kort.
+     
+
+     public static void qualify()
+     {
+         JOptionPane.showMessageDialog(null, "Grattis !! du kvalifiseras för ett creditkort");
+     }
+
+     /**
+      * Metoden noQualify informerar om att de inte kan ta ut ett 
+      * kredit kort
+      
+
+      public static void noQualify()
+      {
+          JOptionPane.showMessageDialog(null, "Sorry du kvalifeceras inte för ett credit kort.");
+      }
+      */
+    
+      
+      myName();
+
+    }
+
+      private static void myName() 
+      {
+          System.out.println("Behram");
+          
+      }
+
+      
+    
+
+    
+}
