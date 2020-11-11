4289df6aa7d33bda4283b9b81d86fcfa6f55c0e6
TallaVa <behram89@gmail.com>
2020-11-11 22:30:39 +0100

    redy for new code

--- src/com/TallaVa/LearningChapter5.java
+++ src/com/TallaVa/LearningChapter5.java
@@ -55,8 +55,9 @@ public class LearningChapter5 {
     
       /**Test av " only a copy of an argument
        * is passed into a method."
-        */
+       */
 
+       /*
         int number = 99;
 
         // display the value in number.
@@ -69,13 +70,13 @@ public class LearningChapter5 {
         //Display the value in number again.
         System.out.println("number is " + number);
         
-      }
+    }
 
       /**
        * The changeMe method accepts an argumet and then
        * changes the value of the parameter.
        */
-      
+      /*
        public static void changeMe(int myValue)
        {
            System.out.println("I am changing the value.");
@@ -85,7 +86,15 @@ public class LearningChapter5 {
 
            //Display the value in myValue.
            System.out.println("Now the value is " + myValue);
-       }
+           */
+
+
+
+
+           
+
+    }
+   
     
 
     
