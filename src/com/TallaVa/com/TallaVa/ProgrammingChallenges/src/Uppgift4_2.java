e8db9cc69f7525997f356e72c6e17cf066362ee9
TallaVa <behram89@gmail.com>
2020-11-16 23:11:09 +0100

    Uppdaterade Uppgift4_2 med övning på slutet av ch5

--- src/com/TallaVa/com/TallaVa/ProgrammingChallenges/src/Uppgift4_2.java
+++ src/com/TallaVa/com/TallaVa/ProgrammingChallenges/src/Uppgift4_2.java
@@ -2,12 +2,53 @@ package com.TallaVa.ProgrammingChallenges.src;
 
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 public class Uppgift4_2 {
+    
+    public static void main(String[] args)
+    {
+      
+        double number;     // håller numer 
+        double total;      // minne 
 
-    public static void main(String [] args) throws IOException {
+        // få nummer från användaren
+        number = getNumber();
 
-        System.out.println("YOOO");
+        // Räkna ut 
+        total = timesTen(number);
+
+        //presentera
+        displayMessage(total);
+        System.exit(0);
+               
     }
     
+    public static double getNumber()
+    {
+        String input;   //användarens input
+        double number;  // hålla nummret
+    // läs in numret
+    input = JOptionPane.showInputDialog("Lägg in ett tal: ");
+        
+    //konverterar inmatningen till en double.
+    number = Double.parseDouble(input);
+
+    //returnera resultatet
+    return number;
     
+    }
+
+    public static double timesTen(double number)
+    {
+        return number * 10.0;
+    }
+
+    public static void displayMessage(double total)
+    {
+        // visar totalen
+        JOptionPane.showMessageDialog(null, "Det totala blir " + total);
+
+    }
+
 }
