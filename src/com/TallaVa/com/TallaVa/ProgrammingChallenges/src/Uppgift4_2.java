5aa076178a912eddc6951b2d8f3176b029eab84f
TallaVa <behram89@gmail.com>
2020-11-17 10:50:55 +0100

    klar med uppgift timesTen.java

--- src/com/TallaVa/com/TallaVa/ProgrammingChallenges/src/Uppgift4_2.java
+++ src/com/TallaVa/com/TallaVa/ProgrammingChallenges/src/Uppgift4_2.java
@@ -23,6 +23,10 @@ public class Uppgift4_2 {
         System.exit(0);
                
     }
+    /**I get Number metoden så läser vi in vad
+     * användaren matar in och sparar det i number
+     * @return number skickar tillbaka värdet
+     */
     
     public static double getNumber()
     {
@@ -39,10 +43,23 @@ public class Uppgift4_2 {
     
     }
 
+    /**
+     * Metoden timesTen räknar ut ekvationen och tilldelar
+     * number ett värde
+     * @param number håller värder som ska in i ekvationen
+     * @return  returnerar värdet efter ekvationen till metoden
+     */
+
     public static double timesTen(double number)
     {
         return number * 10.0;
     }
+
+    /**
+     * Metoden displayMessage ska vissa resultatet
+     * @param total är en parameter som kommer att visa
+     * slutresultatet efter ekvationen
+     */
 
     public static void displayMessage(double total)
     {
