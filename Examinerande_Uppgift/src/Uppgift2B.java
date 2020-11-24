1f0c9c936902a721a6abf65ca1cb63a0567b0327
TallaVa <behram89@gmail.com>
2020-11-24 13:44:58 +0100

    lite problem kvar :(

--- Examinerande_Uppgift/src/Uppgift2B.java
+++ Examinerande_Uppgift/src/Uppgift2B.java
@@ -1,9 +1,9 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Uppgift2B 
 {
-    public static void main (String [] args) throws IOException
+    public static void main (String [] args) throws FileNotFoundException
     {
 
         /**
@@ -13,26 +13,43 @@ public class Uppgift2B
          * applikationen avgöra vilket tal som var störst och vilket som var minst.
          */
 
-        // Skapa en skanner
-         Scanner keyboard = new Scanner(System.in);
-
-         
-
-        // Skapa filen som ska hålla användarens inmatning
-        PrintWriter outputFile = new PrintWriter("Tal2.txt");
-
-        outputFile.println("3.455;1.67;83.98;0.1;23.178");
-
-        File file = new File("tal2");
-        Scanner inputFile = new Scanner(file);
-
-        //Read the file
-        String line = inputFile.nextLine();
-
-        System.out.println(line);
+        // Variabler
+        String mem; // håller ett minne av uppdelningen
+        double number;  // håller sedan värdet i double format
+        double stor = 0.0;
+        double liten = 0.0;
         
+        // Skapa en skanner
+        Scanner keyboard = new Scanner(System.in);
+        
+        // Öppnar filen vi har skapat på skrivbordet
+        File file = new File("C:\\Users\\bere\\IdeaProjects\\Uppgift_2\\Examinerande_Uppgift\\tal2.txt");
+        Scanner inputFile = new Scanner(file);      // Skapar scanner som skannar filen
+        inputFile.useDelimiter(";");                // Använder oss av en Dlimiter som är satt på ";"
+        mem = inputFile.next();
+        number = Double.parseDouble(mem);
+        stor = number;
+        liten = stor;
 
-        outputFile.close();
+        while(inputFile.hasNext())
+        { 
+            double test = number.nextDouble();
+            stor = Math.max(stor, test);
+            liten = Math.min(liten, test);
+        }
+                        
+
+        /** Loop som ska läsa tills det inte finns något mer att läsa 
+        while(inputFile.hasNext())
+        {          
+            stor = Math.max(stor, number);
+            liten = Math.min(liten, number);         
+                    
+            System.out.println(number);
+        }
+        */
+    
+        System.out.println(stor + " och " + liten);
         inputFile.close();
         keyboard.close();
 
